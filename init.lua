8958b21736aea91d9dbe2e041699b1b666793175
Darren Roscoe <droscoe@predictap.com>
2021-10-10 11:34:26 -0700

    Run lua fmt on all code

--- init.lua
+++ init.lua
@@ -6,13 +6,15 @@ local cmd = vim.cmd
 
 local function map(mode, lhs, rhs, opts)
   local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -------------------------------------------------------------------
 -- load plugins
-require('plugins')
+require("plugins")
 
 -------------------------------------------------------------------
 -- Settings
@@ -25,18 +27,17 @@ cmd([[if has('unnamedplus')
 endif
 ]])
 
-
 -- Enable invisible characters
-opt.listchars =  {space='·', tab='>-'}
+opt.listchars = {space = "·", tab = ">-"}
 opt.list = true
 
 -- disable swap files
 opt.swapfile = false
 
 -- write files as they are, don't mess with line endings etc.
-opt.encoding = 'utf-8'
-opt.fileencoding = 'utf-8'
-opt.fileencodings = {'utf-8'}
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+opt.fileencodings = {"utf-8"}
 opt.bomb = true
 opt.binary = true
 
@@ -44,8 +45,7 @@ opt.hlsearch = true -- highlight search
 opt.cursorline = true --highlight the current line
 
 opt.hidden = true -- don't complain when hiding unedited files
-opt.fileformats = {'unix', 'dos', 'mac'}
-
+opt.fileformats = {"unix", "dos", "mac"}
 
 -- TODO do I need syntax on?
 
@@ -61,26 +61,26 @@ opt.modeline = true
 opt.modelines = 10
 
 opt.title = true
-opt.titleold = 'Terminal'
-opt.titlestring='%F'
+opt.titleold = "Terminal"
+opt.titlestring = "%F"
 
-opt.statusline="%F%m%r%h%w%=(%{&ff}/%Y) (line %l/%L, col %c) "
+opt.statusline = "%F%m%r%h%w%=(%{&ff}/%Y) (line %l/%L, col %c) "
 
 -- Make search case insensitive, but become sensitive if an upper case
 -- character is used.
 opt.ignorecase = true
 opt.smartcase = true
 opt.spelllang = "en_us"
-opt.spellfile = fn.stdpath('config') .. "/spell/en.utf-8.add"
+opt.spellfile = fn.stdpath("config") .. "/spell/en.utf-8.add"
 
 opt.undofile = true
-opt.undodir = fn.stdpath('config') .. "/undo"
+opt.undodir = fn.stdpath("config") .. "/undo"
 
 cmd("highlight ExtraWhitespace ctermbg=red guibg=red")
 cmd("match ExtraWhitespace /\\s\\+$/")
 
-opt.mouse="a"
-for i,c in ipairs({'<', '>', 'h', 'l', '[', ']'}) do
+opt.mouse = "a"
+for i, c in ipairs({"<", ">", "h", "l", "[", "]"}) do
   opt.whichwrap:append(c)
 end
 
@@ -90,92 +90,93 @@ opt.shiftwidth = 0
 opt.expandtab = true
 
 -- TODO determine if these can be done more natively
-cmd([[
+cmd(
+  [[
 filetype on
 filetype plugin on
 filetype indent on
 
 autocmd FileType make set noexpandtab tabstop=8 shiftwidth=8 softtabstop=0
-]])
+]]
+)
 
 -- find/replace previews
 opt.inccommand = "split"
 
 opt.autoread = true
 
-
 ------------------------------------------------------------------------
 -- Mappings
 
-vim.g.mapleader = ','
+vim.g.mapleader = ","
 
 -- some command shortcuts
-cmd('cnoreabbrev W! w!')
-cmd('cnoreabbrev Q! q!')
-cmd('cnoreabbrev Qall! qall!')
-cmd('cnoreabbrev Wq wq')
-cmd('cnoreabbrev Wa wa')
-cmd('cnoreabbrev wQ wq')
-cmd('cnoreabbrev WQ wq')
-cmd('cnoreabbrev W w')
-cmd('cnoreabbrev Q q')
-cmd('cnoreabbrev Qall qall')
+cmd("cnoreabbrev W! w!")
+cmd("cnoreabbrev Q! q!")
+cmd("cnoreabbrev Qall! qall!")
+cmd("cnoreabbrev Wq wq")
+cmd("cnoreabbrev Wa wa")
+cmd("cnoreabbrev wQ wq")
+cmd("cnoreabbrev WQ wq")
+cmd("cnoreabbrev W w")
+cmd("cnoreabbrev Q q")
+cmd("cnoreabbrev Qall qall")
 
 -- Search mappings: These will make it so that going to the next one in a
 -- search will center on the line it's found in.
-map('n', 'n', 'nzzzv')
-map('n', 'N', 'Nzzzv')
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
 
 -- splitting
-map('n', '<leader>sp', ':<C-u>split<CR>')
-map('n', '<leader>v', ':<C-u>vsplit<CR>')
+map("n", "<leader>sp", ":<C-u>split<CR>")
+map("n", "<leader>v", ":<C-u>vsplit<CR>")
 -- pbcopy for OSX copy/paste
-map('v', '<C-x>', ':!pbcopy<CR>')
-map('v', '<C-c>',  ':w !pbcopy<CR><CR>')
+map("v", "<C-x>", ":!pbcopy<CR>")
+map("v", "<C-c>", ":w !pbcopy<CR><CR>")
 
 -- buffer movement
-map('n', '<leader>q', ':bp<CR>')
-map('n', '<leader>w', ':bn<CR>')
-map('n', '<leader>x', ':bd<CR>')
+map("n", "<leader>q", ":bp<CR>")
+map("n", "<leader>w", ":bn<CR>")
+map("n", "<leader>x", ":bd<CR>")
 
 -- clear search highlights
-map('n', '<leader><space>', ':noh<cr>', {silent = true})
+map("n", "<leader><space>", ":noh<cr>", {silent = true})
 
 -- window switching
-map('n', '<C-j>', '<C-w>j')
-map('n', '<C-k>', '<C-w>k')
-map('n', '<C-l>', '<C-w>l')
-map('n', '<C-h>', '<C-w>h')
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+map("n", "<C-h>", "<C-w>h")
 
 -- maintain selection while indenting
-map('v', '<', '<gv')
-map('v', '>', '>gv')
+map("v", "<", "<gv")
+map("v", ">", ">gv")
 
-cmd('nmap ; :')
+cmd("nmap ; :")
 
-map('n', '<leader>nt', ':NERDTreeToggle<CR>')
-map('n', '<leader>nf', ':NERDTreeFind %<CR>')
+map("n", "<leader>nt", ":NERDTreeToggle<CR>")
+map("n", "<leader>nf", ":NERDTreeFind %<CR>")
 
-map('n', '<leader>p', ':FZF<CR>')
-map('n', '<leader>P', ':FZF!<CR>')
-
+map("n", "<leader>p", ":FZF<CR>")
+map("n", "<leader>P", ":FZF!<CR>")
 
 -- easy search/replace
-map('n', '<leader>sr', ':%s//gc<left><left><left>')
+map("n", "<leader>sr", ":%s//gc<left><left><left>")
 
-map('n', '<leader>ln', ':try<bar>lnext<bar>catch /^Vim\\%((\\a\\+)\\)\\=:E\\%(553\\<bar>42\\):/<bar>lfirst<bar>endtry<cr>')
-map('n', '<leader>lc', ':lclose<cr>')
-map('n', '<leader>lo', ':lopen<cr>')
+map(
+  "n",
+  "<leader>ln",
+  ":try<bar>lnext<bar>catch /^Vim\\%((\\a\\+)\\)\\=:E\\%(553\\<bar>42\\):/<bar>lfirst<bar>endtry<cr>"
+)
+map("n", "<leader>lc", ":lclose<cr>")
+map("n", "<leader>lo", ":lopen<cr>")
 
 --[[ css colors
 map('n', '<leader>ch', ':ColorHighlight<cr>')
 map('n', '<leader>cc', ':ColorClear<cr>')
 ]]
-
 -- sort object keys
-map('n', '<leader>sk', '[{lv]}k:sort i<cr>')
-
+map("n", "<leader>sk", "[{lv]}k:sort i<cr>")
 
 -- fzf specific, consider moving to dedicated module
-map('n', '<leader>ag', ':Ag<cr>')
-
+map("n", "<leader>ag", ":Ag<cr>")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,194 +1,201 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 function map(mode, lhs, rhs, opts)
-  local options = {noremap = true, silent=true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
+  local options = {noremap = true, silent = true}
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+return require("packer").startup(
+  function()
+    -- Packer can manage itself
+    use "wbthomason/packer.nvim"
 
-return require('packer').startup(function()
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Nerdtree
+    use "scrooloose/nerdtree"
+    use "jistr/vim-nerdtree-tabs"
+    use "Xuyuanp/nerdtree-git-plugin"
+    use "PhilRunninger/nerdtree-visual-selection"
 
-  -- Nerdtree
-  use 'scrooloose/nerdtree'
-  use 'jistr/vim-nerdtree-tabs'
-  use 'Xuyuanp/nerdtree-git-plugin'
-  use 'PhilRunninger/nerdtree-visual-selection'
+    -- QOL stuff
+    use "tpope/vim-commentary"
+    use "tpope/vim-sleuth"
+    use "tpope/vim-surround"
+    use "tpope/vim-fugitive"
+    use "tpope/vim-endwise"
+    use "rstacruz/vim-closer"
+    use "airblade/vim-gitgutter"
 
-  -- QOL stuff
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-sleuth'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-endwise'
-  use 'rstacruz/vim-closer'
-  use 'airblade/vim-gitgutter'
+    -- fuzzy finding
+    use "junegunn/fzf"
+    use "junegunn/fzf.vim"
 
-  -- fuzzy finding
-  use 'junegunn/fzf'
-  use 'junegunn/fzf.vim'
+    -- colorscheme
+    use "nanotech/jellybeans.vim"
 
-  -- colorscheme
-  use 'nanotech/jellybeans.vim'
-
-  use {
-    'vim-airline/vim-airline',
-    requires = 'vim-airline/vim-airline-themes',
-    config = function()
-      vim.g["airline_theme"] = 'jellybeans'
-      vim.g["airline#extensions#syntastic#enabled"] = 1
-      vim.g["airline#extensions#branch#enabled"] = 1
-      vim.g["airline#extensions#tabline#enabled"] = 1
-      vim.g["airline#extensions#tagbar#enabled"] = 1
-      vim.g["airline_skip_empty_sections"] = 1
-      vim.g["airline#extensions#tabline#ignore_bufadd_pat"] = '!|nerd_tree|undotree'
-    end
-  }
-
-
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-    config = function ()
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = "maintained",
-        highlight = {
-          enable = true
-        },
-        indent = {
-          enable = true
-        }
-      })
-    end
-  }
-
-  use {
-    'neovim/nvim-lspconfig',
-    requires = 'hrsh7th/cmp-nvim-lsp',
-    config = function ()
-
-      -- See `:help vim.lsp.*` for documentation on any of the below functions
-      map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-      map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-      map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
-      map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-      map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-      map('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-      map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-      map('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-      map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-      map('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
-      map('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
-      map('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
-      map('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>')
-      map('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-
-      local lspconfig = require('lspconfig')
-      local servers = { 'pyright', 'rust_analyzer', 'tsserver', 'tflint', 'dockerls' }
-      local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-      capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-      for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup {
-          capabilities = capabilities,
-          flags = {
-            debounce_text_changes = 150,
-          }
-        }
+    use {
+      "vim-airline/vim-airline",
+      requires = "vim-airline/vim-airline-themes",
+      config = function()
+        vim.g["airline_theme"] = "jellybeans"
+        vim.g["airline#extensions#syntastic#enabled"] = 1
+        vim.g["airline#extensions#branch#enabled"] = 1
+        vim.g["airline#extensions#tabline#enabled"] = 1
+        vim.g["airline#extensions#tagbar#enabled"] = 1
+        vim.g["airline_skip_empty_sections"] = 1
+        vim.g["airline#extensions#tabline#ignore_bufadd_pat"] = "!|nerd_tree|undotree"
       end
+    }
 
-      lspconfig.html.setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
-      }
-    end
-  }
-  use {'hrsh7th/nvim-cmp',
-    requires = {'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'hrsh7th/cmp-buffer'},
-    config = function()
-      -- Set completeopt to have a better completion experience
-      vim.o.completeopt = 'menu,menuone,noselect'
-      -- luasnip setup
-      local luasnip = require 'luasnip'
+    use {
+      "nvim-treesitter/nvim-treesitter",
+      run = ":TSUpdate",
+      config = function()
+        require("nvim-treesitter.configs").setup(
+          {
+            ensure_installed = "maintained",
+            highlight = {
+              enable = true
+            },
+            indent = {
+              enable = true
+            }
+          }
+        )
+      end
+    }
 
-      -- nvim-cmp setup
-      local cmp = require 'cmp'
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = {
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.close(),
-          ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          },
-          ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
-            else
-              fallback()
-            end
-          end,
-          ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
-            else
-              fallback()
-            end
-          end,
-        },
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'buffer' }
-        },
-      })
-    end
-  }
+    use {
+      "neovim/nvim-lspconfig",
+      requires = "hrsh7th/cmp-nvim-lsp",
+      config = function()
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+        map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+        map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+        map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+        map("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+        map("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+        map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+        map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+        map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+        map("n", "<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+        map("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
+        map("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
+        map("n", "<leader>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>")
+        map("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
 
-  use {'mhartington/formatter.nvim',
-    config = function ()
-      require('formatter').setup({
-        filetype = {
-    lua = {
-        -- luafmt
-        function()
-          return {
-            exe = "luafmt",
-            args = {"--indent-count", 2, "--stdin"},
-            stdin = true
+        local lspconfig = require("lspconfig")
+        local servers = {"pyright", "rust_analyzer", "tsserver", "tflint", "dockerls"}
+        local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+        capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+        for _, lsp in ipairs(servers) do
+          lspconfig[lsp].setup {
+            capabilities = capabilities,
+            flags = {
+              debounce_text_changes = 150
+            }
           }
         end
-    },          rust = {
-            -- Rustfmt
-            function()
-              return {
-                exe = "rustfmt",
-                args = {"--emit=stdout"},
-                stdin = true
-              }
-            end
-          },
+
+        lspconfig.html.setup {
+          capabilities = capabilities,
+          on_attach = on_attach
         }
-      })
-      map('n', '<leader>f', ':Format<CR>')
-    end
-  }
-end)
+      end
+    }
+    use {
+      "hrsh7th/nvim-cmp",
+      requires = {"L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip", "hrsh7th/cmp-buffer"},
+      config = function()
+        -- Set completeopt to have a better completion experience
+        vim.o.completeopt = "menu,menuone,noselect"
+        -- luasnip setup
+        local luasnip = require "luasnip"
 
+        -- nvim-cmp setup
+        local cmp = require "cmp"
+        cmp.setup(
+          {
+            snippet = {
+              expand = function(args)
+                luasnip.lsp_expand(args.body)
+              end
+            },
+            mapping = {
+              ["<C-p>"] = cmp.mapping.select_prev_item(),
+              ["<C-n>"] = cmp.mapping.select_next_item(),
+              ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+              ["<C-f>"] = cmp.mapping.scroll_docs(4),
+              ["<C-Space>"] = cmp.mapping.complete(),
+              ["<C-e>"] = cmp.mapping.close(),
+              ["<CR>"] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = true
+              },
+              ["<Tab>"] = function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif luasnip.expand_or_jumpable() then
+                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+                else
+                  fallback()
+                end
+              end,
+              ["<S-Tab>"] = function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+                else
+                  fallback()
+                end
+              end
+            },
+            sources = {
+              {name = "nvim_lsp"},
+              {name = "luasnip"},
+              {name = "buffer"}
+            }
+          }
+        )
+      end
+    }
 
-
+    use {
+      "mhartington/formatter.nvim",
+      config = function()
+        require("formatter").setup(
+          {
+            filetype = {
+              lua = {
+                -- luafmt
+                function()
+                  return {
+                    exe = "luafmt",
+                    args = {"--indent-count", 2, "--stdin"},
+                    stdin = true
+                  }
+                end
+              },
+              rust = {
+                -- Rustfmt
+                function()
+                  return {
+                    exe = "rustfmt",
+                    args = {"--emit=stdout"},
+                    stdin = true
+                  }
+                end
+              }
+            }
+          }
+        )
+        map("n", "<leader>f", ":Format<CR>")
+      end
+    }
+  end
+)
