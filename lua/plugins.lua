64f4d76e2b28e468a0a2726170312fc59ec9960d
Darren Roscoe <droscoe@predictap.com>
2023-05-03 23:37:59 -0700

    Add copilot

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,6 +25,26 @@ return require("packer").startup(
 
     use "andreshazard/vim-freemarker"
 
+    -- Copilot
+    use {
+      "github/copilot.vim",
+      config = function()
+        vim.g.copilot_no_tab_map = true
+        vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', {silent = true, expr = true})
+        vim.g.copilot_filetypes = {
+          ["*"] = false,
+          ["javascript"] = true,
+          ["javascriptreact"] = true,
+          ["typescript"] = true,
+          ["typescriptreact"] = true,
+          ["sql"] = true,
+          ["lua"] = false,
+          ["rust"] = true,
+          ["go"] = true,
+          ["python"] = true
+        }
+      end
+    }
     -- Nerdtree
     use {
       "scrooloose/nerdtree",
