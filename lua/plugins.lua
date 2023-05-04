e89697f81e2e7381cc92ef30716fd564c99db0ef
Darren Roscoe <droscoe@predictap.com>
2023-05-04 00:09:16 -0700

    Add rust-tools.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -241,6 +241,24 @@ return require("packer").startup(
         }
       end
     }
+    use {
+      "simrat39/rust-tools.nvim",
+      config = function()
+        local rt = require("rust-tools")
+        rt.setup(
+          {
+            server = {
+              on_attach = function(_, bufnr)
+                -- Hover actions
+                vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, {buffer = bufnr})
+                -- Code action groups
+                vim.keymap.set("n", "<Leader>ac", rt.code_action_group.code_action_group, {buffer = bufnr})
+              end
+            }
+          }
+        )
+      end
+    }
 
     use {
       "hrsh7th/nvim-cmp",
